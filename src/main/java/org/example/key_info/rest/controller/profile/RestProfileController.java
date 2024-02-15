0425a2c4b0289a3e8fa38be3cbf93024fe86f893
T9404 <ghubman1@gmail.com>
2024-02-15 19:57:04 +0700

    small update profile endpoints

--- src/main/java/org/example/key_info/rest/controller/profile/RestProfileController.java
+++ src/main/java/org/example/key_info/rest/controller/profile/RestProfileController.java
@@ -1,5 +1,6 @@
 package org.example.key_info.rest.controller.profile;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.key_info.public_interface.profile.ProfileDto;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Для работы с профилем пользователя")
 public class RestProfileController {
 
+    @Operation(summary = "Получить свой профиль")
     @GetMapping()
     public ResponseEntity<ProfileDto> getMyProfile(@RequestHeader("Authorization") String accessToken) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
+    @Operation(summary = "Обновить свой профиль")
     @PutMapping()
     public ResponseEntity<ProfileDto> updateMyProfile(@RequestHeader("Authorization") String accessToken,
                                                       @RequestBody ProfileDto dto) {
