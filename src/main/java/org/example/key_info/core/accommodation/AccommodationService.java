ac9e46fce1a9b22bebc5d7c38b87c00db895cad1
T9404 <ghubman1@gmail.com>
2024-02-17 18:59:28 +0700

    fixed problem accommodation service

--- src/main/java/org/example/key_info/core/accommodation/AccommodationService.java
+++ src/main/java/org/example/key_info/core/accommodation/AccommodationService.java
@@ -2,6 +2,7 @@ package org.example.key_info.core.accommodation;
 
 import lombok.RequiredArgsConstructor;
 import org.example.key_info.core.client.repository.ClientRole;
+import org.example.key_info.core.key.repository.KeyRepository;
 import org.example.key_info.public_interface.accommodation.AccommodationCreateDto;
 import org.example.key_info.public_interface.accommodation.AccommodationDeleteDto;
 import org.example.key_info.public_interface.exception.ExceptionInApplication;
@@ -16,7 +17,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class AccommodationService {
     private static final List<ClientRole> ROLES_CONTROL_ACCOMMODATION_LIFECYCLE = List.of(ClientRole.DEANERY);
-
+    private final KeyRepository keyRepository;
     private final AccommodationRepository accommodationRepository;
 
     public void createAccommodation(AccommodationCreateDto dto) {
@@ -29,6 +30,10 @@ public class AccommodationService {
 
     public void deleteAccommodation(AccommodationDeleteDto dto) {
         validateClientRoles(dto.clientRoles());
+
+        checkExistenceKey(dto.buildId(), dto.roomId());
+
+        checkExistenceAccommodation(dto.buildId(), dto.roomId());
 
         accommodationRepository.deleteAccommodation(dto.buildId(), dto.roomId());
     }
@@ -52,5 +57,21 @@ public class AccommodationService {
         return clientRoles
                 .stream()
                 .anyMatch(ROLES_CONTROL_ACCOMMODATION_LIFECYCLE::contains);
+    }
+
+    private void checkExistenceKey(int buildId, int roomId) {
+        var keys = keyRepository.getAllKeys(buildId, roomId);
+
+        if (!keys.isEmpty()) {
+            throw new ExceptionInApplication("Нельзя удалить аудиторию, в которой есть ключи", ExceptionType.INVALID);
+        }
+    }
+
+    private void checkExistenceAccommodation(int buildId, int roomId) {
+        var accommodation = accommodationRepository.getAccommodation(buildId, roomId);
+
+        if (accommodation.isEmpty()) {
+            throw new ExceptionInApplication("Аудитория не найдена", ExceptionType.INVALID);
+        }
     }
 }
