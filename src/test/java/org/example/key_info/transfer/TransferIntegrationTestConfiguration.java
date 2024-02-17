9210ef5f9048e2afd7c8eb0d6e96e5f99cb1f9b5
Gordey Dovydenko <inostaziss@gmail.com>
2024-02-17 10:49:35 +0700

    added transfer test

---
+++ src/test/java/org/example/key_info/transfer/TransferIntegrationTestConfiguration.java
@@ -0,0 +1,19 @@
+package org.example.key_info.transfer;
+
+import org.example.key_info.core.client.repository.ClientRepositoryImpl;
+import org.example.key_info.core.key.repository.KeyRepositoryImpl;
+import org.example.key_info.core.transfer.TransferService;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@JooqTest
+@ImportAutoConfiguration({
+        TransferService.class,
+        ClientRepositoryImpl.class,
+        KeyRepositoryImpl.class
+})
+public class TransferIntegrationTestConfiguration {
+
+}
