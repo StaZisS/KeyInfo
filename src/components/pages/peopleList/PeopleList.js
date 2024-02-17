86fbc7c88cb8e8cbb9e24fcf28e781afd269d886
Aleksandr Osipov <otrijka04osip@gmail.com>
2024-02-17 16:09:33 +0700

    Update people design -people item -people page

--- src/components/pages/peopleList/PeopleList.js
+++ src/components/pages/peopleList/PeopleList.js
@@ -1,24 +1,32 @@
-import { PeopleItem } from "./people/peopleItem";
+import {PeopleItem} from "./people/peopleItem";
+import {Card, CardBody, CardHeader, CardTitle, Container} from "react-bootstrap";
 
 
-    // нужен запрос
-    const peopleData = [
-        { name: 'Иванов Юрий Петрович' },
-        { name: 'Порпоппо ОПП ЛПНПнп' },
-        { name: 'ПРПР рпПп РПНПНРПРО' },
-        { name: 'врврвр вроврврв воврвр вовр' }
-    ];
+// нужен запрос
+const peopleData = [
+    {name: 'Иванов Юрий Петрович'},
+    {name: 'Порпоппо ОПП ЛПНПнп'},
+    {name: 'ПРПР рпПп РПНПНРПРО'},
+    {name: 'врврвр вроврврв воврвр вовр'}
+];
 
 export const PeopleList = () => {
     return (
-        <div className="col-10 col-md-8 mx-auto mt-3 bg-white p-4 shadow border-0">
-        <h3>Пользователи:</h3>
-            {peopleData.map((Data, index) => (
-                <PeopleItem
-                    key={index} 
-                    name={Data.name} 
-                />
-            ))}
-        </div>
+        <Container className='mt-5'>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Пользователи</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    {peopleData.map((Data, index) => (
+                        <PeopleItem
+                            key={index}
+                            name={Data.name}
+                        />
+                    ))}
+                </CardBody>
+            </Card>
+
+        </Container>
     )
 }
--- src/components/pages/peopleList/people/peopleItem.js
+++ src/components/pages/peopleList/people/peopleItem.js
@@ -1,23 +1,23 @@
-import { ButtonStudent } from './studentRoleButton'
-import { ButtonTeacher } from './teacherRoleButton'
+import {ButtonStudent} from './studentRoleButton'
+import {ButtonTeacher} from './teacherRoleButton'
 
 import('../../../../styles/peopleItem.css')
 
 export const PeopleItem = ({name}) => {
     return (
-        <div className='w-100 d-flex item border flex-wrap'>
-        
-            <div className='d-flex flex-grow-1 h-100 align-items-center'>
-                <div className='ps-3 d-flex align-items-center flex-grow-1 number'>
+        <div className='w-100 d-flex item border flex-nowrap justify-content-between'>
+
+            <div className='d-flex h-100 align-items-center'>
+                <div className='ps-3 d-flex align-items-center flex-grow-0 number'>
                     <span className='name'>{name}</span>
                 </div>
+
             </div>
-            <div className='h-100 d-flex align-items-center'>
-            <ButtonStudent/>
-            <ButtonTeacher/>
+            <div className="buttons d-flex h-100 align-items-center">
+                <ButtonStudent/>
+                <ButtonTeacher/>
             </div>
 
-            
         </div>
     )
 }
