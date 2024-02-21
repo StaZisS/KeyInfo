f6df66e18dda059c7dd446636c5fe55f7a6e8ff0
T9404 <ghubman1@gmail.com>
2024-02-21 12:16:59 +0700

    fixed filter chain and unspecified role

--- backend/src/main/java/org/example/key_info/core/auth/JwtFilter.java
+++ backend/src/main/java/org/example/key_info/core/auth/JwtFilter.java
@@ -1,14 +1,19 @@
 package org.example.key_info.core.auth;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.key_info.core.auth.provider.JwtProvider;
 import org.example.key_info.core.auth.util.JwtUtils;
+import org.example.key_info.public_interface.exception.ExceptionInApplication;
+import org.example.key_info.public_interface.exception.ExceptionType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -31,13 +36,26 @@ public class JwtFilter extends GenericFilterBean {
             fc.doFilter(request, response);
             return;
         }
-        jwtProvider.validateAccessToken(token);
+        try {
+            jwtProvider.validateAccessToken(token);
 
-        final Claims claims = jwtProvider.getAccessClaims(token);
-        final JwtAuthentication jwtInfoToken = JwtUtils.generate(claims);
-        jwtInfoToken.setAuthenticated(true);
-        jwtInfoToken.setUserId(UUID.fromString(claims.getSubject()));
-        SecurityContextHolder.getContext().setAuthentication(jwtInfoToken);
+            final Claims claims = jwtProvider.getAccessClaims(token);
+            final JwtAuthentication jwtInfoToken = JwtUtils.generate(claims);
+            jwtInfoToken.setAuthenticated(true);
+            jwtInfoToken.setUserId(UUID.fromString(claims.getSubject()));
+            SecurityContextHolder.getContext().setAuthentication(jwtInfoToken);
+
+            String url = ((HttpServletRequest) request).getRequestURI();
+
+            if(!url.contains("auth") && jwtInfoToken.getAuthorities().contains(Role.UNSPECIFIED)) {
+                throw new ExceptionInApplication("Access denied for UNSPECIFIED role", ExceptionType.FORBIDDEN);
+            }
+        } catch (ExceptionInApplication e) {
+            response.getWriter().write(e.getMessage());
+            HttpServletResponse hsr = (HttpServletResponse) response;
+            hsr.setStatus(401);
+            return;
+        }
 
         fc.doFilter(request, response);
      }
